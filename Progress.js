082d9429f7d5a4523b6bbf04819bcaf445781f73
Stepan Sukhachev <droiderlinux@yahoo.com>
2024-05-14 19:34:53 +0300

    feat: add styling + responsiveness

--- Progress.js
+++ Progress.js
@@ -5,13 +5,22 @@ class Progress extends HTMLElement {
     }
 
     :host {
+      --color-accent: #005bff;
+      --color-secondary: #e0e6ef;
       position: relative;
       display: flex;
       justify-content: center;
-      gap: 50px;
+      align-items: center;
+      gap: 60px;
       max-width: 568px;
       max-height: 320px;
-      padding: 110px;
+      padding: 110px 50px;
+    }
+
+    @media (max-width: 500px) {
+      :host {
+        flex-direction: column;
+      }
     }
 
     .header {
@@ -21,15 +30,15 @@ class Progress extends HTMLElement {
     }
 
     .progressBar {
-      --size: 100px;
-      --bar-width: 15px;
+      --size: 120px;
+      --bar-width: 20px;
       
       width: var(--size);
       aspect-ratio: 1 / 1;
 
       background: conic-gradient(
-        #005bff var(--progress, 0),
-        #e0e6ef 0%
+        var(--color-accent) var(--progress),
+        var(--color-secondary) 0%
       );
       border-radius: 50%;
 
@@ -50,7 +59,84 @@ class Progress extends HTMLElement {
 
     .settings {
       display: flex;
+      justify-content: space-between;
+      gap: 20px;
       flex-direction: column;
+    }
+
+    .label {
+      display: flex;
+      gap: 20px;
+    }
+
+    .input {
+      font-family: inherit; 
+      color: inherit;
+      border-radius: 15px;
+    }
+
+    .input[type="number"]::-webkit-outer-spin-button,
+    .input[type="number"]::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    
+    .input[type="number"] {
+      -moz-appearance: textfield;
+    }
+
+    .input__value {
+      width: 50px;
+      border: solid black 1px;
+      text-align: center;
+      font-size: 1rem;
+    }
+
+    .input[type="checkbox"] {
+      position: relative;
+      
+      width: 50px;
+      height: 30px;
+      margin: 0;
+    
+      vertical-align: top;
+    
+      background: var(--color-secondary);
+      border-radius: 30px;
+      outline: none;
+      cursor: pointer;
+    
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+        
+      transition: all 0.3s cubic-bezier(0.2, 0.85, 0.32, 1.2);
+    }
+    
+    input[type="checkbox"]::after {
+      content: "";
+      
+      position: absolute;
+      left: 3px;
+      top: 2px;
+      
+      width: 26px;
+      height: 26px;
+      background-color: white;
+      border-radius: 50%;
+      
+      transform: translateX(0);
+      
+      transition: all 0.3s cubic-bezier(0.2, 0.85, 0.32, 1.2);
+    }
+    
+    .input[type="checkbox"]:checked::after {
+      transform: translateX(calc(100% - 7px));
+      background-color: #fff;  
+    }
+    
+    .input[type="checkbox"]:checked {
+      background-color: var(--color-accent);
     }
   `;
 
@@ -81,15 +167,34 @@ class Progress extends HTMLElement {
     progress.append(this.progressBar);
 
     // Settings part
-
+    const valueLabel = document.createElement("label");
+    valueLabel.classList.add('label');
+    const valueSpan = document.createElement("span");
+    valueSpan.textContent = "Value";
     this.valueInput = document.createElement("input");
-    this.valueInput.classList.add("input__value");
+    this.valueInput.classList.add("input", "input__value");
     this.valueInput.type = "number";
+    valueLabel.append(this.valueInput, valueSpan);
+
+    const animateLabel = document.createElement("label");
+    animateLabel.classList.add('label');
+    const animateSpan = document.createElement("span");
+    animateSpan.textContent = "Animate";
     this.animateInput = document.createElement("input");
+    this.animateInput.classList.add("input");
     this.animateInput.type = "checkbox";
+    animateLabel.append(this.animateInput, animateSpan);
+
+    const hideLabel = document.createElement("label");
+    hideLabel.classList.add('label');
+    const hideSpan = document.createElement("span");
+    hideSpan.textContent = "Hide";
     this.hideInput = document.createElement("input");
+    this.hideInput.classList.add("input");
     this.hideInput.type = "checkbox";
-    settings.append(this.valueInput, this.animateInput, this.hideInput);
+    hideLabel.append(this.hideInput, hideSpan);
+
+    settings.append(valueLabel, animateLabel, hideLabel);
 
     // Combining alltogether
 
@@ -146,7 +251,6 @@ class Progress extends HTMLElement {
   // all logic here:
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log(name, oldValue, newValue);
     switch (name) {
       case "value":
         this.progressBar.style.setProperty("--progress", this.value + "%");
